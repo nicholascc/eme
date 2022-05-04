d160e9174b7b3028ad9282c8988b3efc6e9cd14c
nicholascc <charettenr@gmail.com>
2022-05-04 13:30:07 -0500

    Fixed relative path resolution.
    This is temporary; need a better solution later.

--- src/files.c
+++ src/files.c
@@ -7,17 +7,54 @@
 #include "c-utils/darray.h"
 #include "errors.h"
 
-char *concat(char *a, char *b) {
+char *concat_path(char *a, char *b, Location loc) {
   int size = 0;
   while(a[size++] != 0);
-  int a_size = size;
+  int a_size = size-1;
   while(b[size++ - a_size] != 0);
+  int b_size = size-a_size-1;
 
-  char *c = malloc(size-1);
-  for(int i = 0; i < a_size-1; i++) c[i] = a[i];
-  for(int i = 0; i < size-a_size-1; i++) c[i+a_size-1] = b[i];
-  c[size-2] = 0;
-  return c;
+  int s_size = size-1;
+  char *s = malloc(s_size+1);
+  for(int i = 0; i < a_size; i++) s[i] = a[i];
+  for(int i = 0; i < b_size; i++) s[i+a_size] = b[i];
+  s[s_size] = 0;
+
+  int i = 0;
+  int j = 0;
+  char c;
+  while(i < size) {
+    c = s[i];
+    int section_start = i;
+    bool current_is_back = true;
+    int current_dot_count = 0;
+    while(i < size) {
+      c = s[i];
+      s[j] = c;
+      if(c == '\\' || c == '/') break;
+      if(c == '.')
+        current_dot_count++;
+      else
+        current_is_back = false;
+      i++;
+      j++;
+    }
+
+    if(current_is_back) {
+      j--;
+      while(j >= 0 && current_dot_count != 0) {
+        if(s[j] == '\\' || s[j] == '/') current_dot_count--;
+        j--;
+      }
+      if(j >= 0) j++;
+    }
+
+    i++;
+    j++;
+  }
+  s[j] = 0;
+
+  return s;
 }
 
 char *os_load_file(char *filename, Location loc) {
@@ -48,7 +85,7 @@ void init_file_array(void) {
 }
 
 int add_file(char *root_directory, char *filename, Location loc) {
-  char *full_filename = concat(root_directory, filename);
+  char *full_filename = concat_path(root_directory, filename, loc);
   for(int i = 0; i < files.length; i++) {
     if(strcmp(full_filename, files.data[i].filename) == 0) {
       return i;
